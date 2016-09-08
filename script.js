e427b9d3952da1d6d5b08797ec368069491392a5
tinylittleburger <tinylittleburger@gmail.com>
2016-09-08 07:51:39 +0200

    Access token extracted from fragment (script)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
  if(window.location.hash)
  {
-      var hash = window.location.hash.substring(1);
-      alert (hash);
+    var hash = window.location.hash.substring(1);
+	var item = hash.split("=");
+	token = decodeURIComponent(item[1]);
+	  
+    alert (token);
  }
