1950c6ab9e3a8533a06c401d6a6abd3e560c7e66
tinylittleburger <tinylittleburger@gmail.com>
2016-09-08 18:59:25 +0200

    JSONP used for script

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
- if(window.location.hash)
+ function OnResult(result)
+ {
+	 alert(result);
+ }
+ 
+ if (window.location.hash)
  {
     var hash = window.location.hash.substring(1);
 	var item = hash.split("=");
 	token = decodeURIComponent(item[1]);
-	  
-    alert (token);
+	
+	script = document.createElement("newScript");
+	script.src = "https://api.instagram.com/v1/tags/coffee/media/recent?access_token=" + token + "&callback=OnResult";
+	document.body.appendChild(script);
  }
